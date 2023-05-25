5e44e1d4cd48470e5ce09d6dd1b293fa301e8b21
Davis <davis.stegmanis@helmes.com>
2023-05-25 13:57:20 +0300

    Project init

--- Kapu Patrula/src/App.vue
+++ Kapu Patrula/src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Iesniegt sludzinājumu</RouterLink>
+        <RouterLink to="/about">Sazināties ar operātoru</RouterLink>
       </nav>
     </div>
   </header>
--- Kapu Patrula/src/components/TheWelcome.vue
+++ Kapu Patrula/src/components/TheWelcome.vue
@@ -5,82 +5,122 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import Form from 'vform'
+
 </script>
-
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <el-row>
+    <el-col :span="24">
+      <el-steps :active="activeStep" finish-status="success" align-center>
+        <el-step title="Informācija par Jums" />
+        <el-step title="Kas noticis?" />
+        <el-step title="Kur noticis?" />
+        <el-step title="Papildus informācija" />
+      </el-steps>
+    </el-col>
+  </el-row>
+  <br>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+  <div class="sludinajuma-forma">
+    <el-form :model="form" label-width="120px">
+    <el-collapse v-model="step" @change="handleChange">
+      <el-collapse-item title="Informācija par Jums" name="1">
+        <div>
+          <el-alert title="Šī informācija mums ir nepieciešama, lai ar Jums spētu sazināties Mūsu operātors" type="info" />
+          <br>
+        </div>
+        <div>
+          <el-form-item label="Jūsu vārds">
+            <el-input v-model="form.firstname" />
+          </el-form-item>
+          <el-form-item label="Tālrunis">
+            <el-input v-model="form.phone" />
+          </el-form-item>
+          <el-form-item label="E-pasts">
+            <el-input v-model="form.email" />
+          </el-form-item>
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Kas noticis?" name="2">
+        <div>
+          Operation feedback: enable the users to clearly perceive their
+          operations by style updates and interactive effects;
+        </div>
+        <div>
+          Visual feedback: reflect current state by updating or rearranging
+          elements of the page.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Kur noticis?" name="3">
+        <div>
+          Simplify the process: keep operating process simple and intuitive;
+        </div>
+        <div>
+          Definite and clear: enunciate your intentions clearly so that the
+          users can quickly understand and make decisions;
+        </div>
+        <div>
+          Easy to identify: the interface should be straightforward, which helps
+          the users to identify and frees them from memorizing and recalling.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Papildus informācija" name="4">
+        <div>
+          Decision making: giving advices about operations is acceptable, but do
+          not make decisions for the users;
+        </div>
+        <div>
+          Controlled consequences: users should be granted the freedom to
+          operate, including canceling, aborting or terminating current
+          operation.
+        </div>
+      </el-collapse-item>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+    </el-collapse>
+<div>
+  <br>
+  <el-checkbox v-model="checked2" @change="toggleSubmitButtons($event)">Esmu iepazinies ar lietošanas noteikumiem</el-checkbox>
+  <el-divider/>
+  <el-form-item>
+    <el-button type="success" :disabled="!canSubmit">Iesniegt sludinājumu</el-button>
+    <el-button>Atcelt</el-button>
+  </el-form-item>
+</div>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+    </el-form>
+  </div>
 </template>
+<script>
+import {ElMessage} from 'element-plus'
+export default {
+  name: 'Iesniegt sludinajumu',
+  data() {
+    return {
+      form: new Form({
+        firtname: '',
+        email: '',
+        phone: '',
+      }),
+      step: ['1'],
+      canSubmit: false,
+      activeStep: 4
+    }
+  },
+  props: [
+    'x',
+  ],
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+    toggleSubmitButtons(event){
+      if(event){
+        this.canSubmit = true;
+      } else {
+        this.canSubmit = false;
+      }
+    }
+  }
+}
+</script>
